d2b646a3921bd9fa9e7c21abf070c223203eaada
Adrian <awinf204@gmail.com>
2022-03-04 17:46:45 +0100

    * Change Mode Types

--- app/src/main/java/com/adrc95/marvelappsample/ui/common/ModeType.kt
+++ app/src/main/java/com/adrc95/marvelappsample/ui/common/ModeType.kt
@@ -2,14 +2,15 @@ package com.adrc95.marvelappsample.ui.common
 
 enum class ModeType(val value: Int) {
     DAY(1),
-    NIGHT(2);
+    NIGHT(2),
+    AUTOMATIC(3);
 
     companion object {
-        fun toModeType(value: Int): ModeType? =
+        fun toModeType(value: Int): ModeType =
             when (value) {
                 1 -> DAY
                 2 -> NIGHT
-                else -> null
+                else -> AUTOMATIC
             }
     }
 }
