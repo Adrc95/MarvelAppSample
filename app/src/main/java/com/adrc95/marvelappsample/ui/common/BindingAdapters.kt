4093ec12baf98f973aca41d4230879411c38c66f
Adrian <awinf204@gmail.com>
2022-03-04 17:46:30 +0100

    * Create binding srcMode

--- app/src/main/java/com/adrc95/marvelappsample/ui/common/BindingAdapters.kt
+++ app/src/main/java/com/adrc95/marvelappsample/ui/common/BindingAdapters.kt
@@ -1,10 +1,15 @@
 package com.adrc95.marvelappsample.ui.common
 
+import android.content.res.Configuration
 import android.view.View
 import android.widget.ImageView
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.adrc95.marvelappsample.R
 
 @BindingAdapter("visible")
 fun View.visible(visible: Boolean?) {
@@ -21,4 +26,23 @@ fun ImageView.bindUrl(url: String?) {
 @BindingAdapter("customDivider")
 fun RecyclerView.divider(@DrawableRes drawableRes: Int?) {
    if (drawableRes != null) setDivider(drawableRes)
+}
+
+@BindingAdapter("srcMode")
+fun ImageView.setThemeMode(modeType: ModeType? = ModeType.AUTOMATIC) {
+   val resource = when (modeType) {
+        ModeType.NIGHT -> R.drawable.ic_light_mode
+        ModeType.DAY -> R.drawable.ic_dark_mode
+        else -> {
+            when (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                Configuration.UI_MODE_NIGHT_NO -> R.drawable.ic_dark_mode
+                Configuration.UI_MODE_NIGHT_YES -> R.drawable.ic_light_mode
+                else -> { R.drawable.ic_menu_square}
+            }
+        }
+    }
+    val drawable = ContextCompat.getDrawable(context, resource)
+    drawable?.setTint(ContextCompat.getColor(context, R.color.white))
+    background = drawable
+
 }
