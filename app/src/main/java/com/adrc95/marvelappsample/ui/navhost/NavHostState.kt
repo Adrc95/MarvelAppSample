b0e9432067155349a63bdbc1b5b522cd89cf7f6e
Adrian <awinf204@gmail.com>
2022-03-04 17:47:31 +0100

    * Change NavHostState

--- app/src/main/java/com/adrc95/marvelappsample/ui/navhost/NavHostState.kt
+++ app/src/main/java/com/adrc95/marvelappsample/ui/navhost/NavHostState.kt
@@ -1,17 +1,27 @@
 package com.adrc95.marvelappsample.ui.navhost
 
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatDelegate
 import com.adrc95.marvelappsample.ui.common.ModeType
 
-fun buildNavHostState() = NavHostState()
+fun Activity.buildNavHostState(context: Context = this) = NavHostState(context)
 
-class NavHostState() {
+class NavHostState(private val context: Context) {
 
-    fun onChangeTheme(mode : ModeType?) {
+    fun onChangeTheme(mode : ModeType) {
         when (mode) {
          ModeType.NIGHT -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
          ModeType.DAY ->  AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            else -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_UNSPECIFIED)
+            else -> getAutomaticColor()
+        }
+    }
+
+    private fun getAutomaticColor() {
+        when (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            Configuration.UI_MODE_NIGHT_YES -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
     }
 
