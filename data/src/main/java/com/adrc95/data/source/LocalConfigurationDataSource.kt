b7e561cfbe839d1303f850782bedb1745e374e2a
Adrian <awinf204@gmail.com>
2022-03-04 17:46:10 +0100

    * Delete Imports

--- data/src/main/java/com/adrc95/data/source/LocalConfigurationDataSource.kt
+++ data/src/main/java/com/adrc95/data/source/LocalConfigurationDataSource.kt
@@ -1,9 +1,5 @@
 package com.adrc95.data.source
 
-import arrow.core.Either
-import com.adrc95.data.exception.Failure
-import com.adrc95.domain.Character
-
 interface LocalConfigurationDataSource {
 
     suspend fun getThemeMode(): Int
